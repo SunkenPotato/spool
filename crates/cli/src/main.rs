306185ac282a149652c7aef208aa114137ed2d1a
SunkenPotato <3.14radius02@gmail.com>
2025-02-19 13:28:35 +0100

    feat: command docs

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,35 +1,46 @@
 mod command;
 
-use std::io::{self, Write};
+use std::io::{self, Stderr, Stdin, Stdout, Write};
 
 use command::{register_default_commands, CommandRegistry};
 use spool::{Env, Parsed};
 
 const PROMPT: &str = "→ ";
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub(crate) struct AppState<'e> {
+    pub(crate) stdin: Stdin,
+    pub(crate) stdout: Stdout,
+    pub(crate) stderr: Stderr,
+    pub(crate) env: Env<'e>,
+}
 
 fn main() -> io::Result<()> {
-    let mut command_registry = CommandRegistry::default();
+    let mut app_state = AppState {
+        stdin: io::stdin(),
+        stdout: io::stdout(),
+        stderr: io::stderr(),
+        env: Env::new(),
+    };
+
+    let mut command_registry = CommandRegistry::new()?;
+
     register_default_commands(&mut command_registry);
-
-    let stdin = io::stdin();
-    let mut stdout = io::stdout();
-    let mut stderr = io::stderr();
-
-    let mut env = Env::new();
+    greeting(&mut app_state.stdout)?;
 
     loop {
         let mut input = String::new();
-        write!(stdout, "{}", PROMPT)?;
-        stdout.flush()?;
+        write!(app_state.stdout, "{}", PROMPT)?;
+        app_state.stdout.flush()?;
 
-        stdin.read_line(&mut input)?;
+        app_state.stdin.read_line(&mut input)?;
         input = input.trim().into();
 
         if input.is_empty() {
             continue;
         }
 
-        if let Ok(returns) = command_registry.execute(&input, &mut env) {
+        if let Ok(returns) = command_registry.execute(&input, &mut app_state) {
             match returns {
                 command::CommandReturns::Exit => return Ok(()),
                 _ => continue,
@@ -39,28 +50,38 @@ fn main() -> io::Result<()> {
         let (s, parsed) = match Parsed::parse(&input) {
             Ok(v) => v,
             Err(e) => {
-                write!(stderr, "Parse error: {:?}\n", e)?;
-                stderr.flush()?;
+                write!(app_state.stderr, "Parse error: {:?}\n", e)?;
+                app_state.stderr.flush()?;
                 continue;
             }
         };
 
         if !s.is_empty() {
-            write!(stderr, "Input not fully consumed by parser: {s}\n")?;
-            stderr.flush()?;
+            write!(
+                app_state.stderr,
+                "Input not fully consumed by parser: {s}\n"
+            )?;
+            app_state.stderr.flush()?;
             continue;
         }
 
-        let eval = match parsed.eval(&mut env) {
+        let eval = match parsed.eval(&mut app_state.env) {
             Ok(v) => v,
             Err(e) => {
-                write!(stderr, "Evaluation error: {:?}\n", e)?;
-                stderr.flush()?;
+                write!(app_state.stderr, "Evaluation error: {:?}\n", e)?;
+                app_state.stderr.flush()?;
                 continue;
             }
         };
 
-        write!(stdout, "{:?}\n", eval)?;
-        stdout.flush()?;
+        write!(app_state.stdout, "{:?}\n", eval)?;
+        app_state.stdout.flush()?;
     }
+}
+
+fn greeting(stdout: &mut Stdout) -> io::Result<()> {
+    write!(stdout, "Spool {} on {}\n", VERSION, std::env::consts::OS)?;
+    write!(stdout, "Type '/help' for a list of commands\n")?;
+
+    Ok(())
 }
